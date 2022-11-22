101f1c1f1dafbf2310c604e494fed649057019de
ekrlbn <105484430+ekrlbn@users.noreply.github.com>
2022-11-22 08:07:58 +0300

    test cases for caption router have been created

--- test/caption.js
+++ test/caption.js
@@ -1,10 +1,9 @@
-/* globals describe,after, it */
+/* globals describe, after, it */
 
 const assert = require('assert');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-// eslint-disable-next-line object-curly-newline
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
@@ -13,8 +12,8 @@ const { User, Image } = require('../models/models');
 chai.use(chaiHttp);
 const app = require('../app');
 
-// images
-describe('/images', async () => {
+// captions
+describe('/captions', async () => {
 	const username = crypto.randomBytes(10).toString('hex');
 	let password = crypto.randomBytes(10).toString('hex');
 
@@ -29,7 +28,18 @@ describe('/images', async () => {
 
 	const auth = await jwt.sign(user, process.env.JWT_SECRET);
 
+	const image = await Image.create({
+		filename: 'test',
+		user_id: user.id,
+	});
+
 	after(async () => {
+		await Image.destroy({
+			where: {
+				user_id: user.id,
+			},
+		});
+
 		await User.destroy({
 			where: {
 				username,
@@ -41,7 +51,7 @@ describe('/images', async () => {
 		it('should return 200', async () => {
 			await chai
 				.request(app)
-				.get('/api/images')
+				.get(`/api/captions/${image.id}`)
 				.set('Authorization', `Bearer ${auth}`)
 				.then((res) => {
 					assert.equal(res.status, 200);
@@ -53,7 +63,7 @@ describe('/images', async () => {
 		it('should return 401 if no token is provided', async () => {
 			await chai
 				.request(app)
-				.get('/api/images')
+				.get('/api/captions')
 				.then((res) => {
 					assert.equal(res.status, 401);
 				})
@@ -61,41 +71,11 @@ describe('/images', async () => {
 					throw err;
 				});
 		});
-	});
-	describe('POST', () => {
-		it('should return 201', async () => {
+
+		it('should return 404 if image does not exist', async () => {
 			await chai
 				.request(app)
-				.post('/api/images')
-				.set('Authorization', `Bearer ${auth}`)
-				.attach('file', 'test/test.png')
-				.then((res) => {
-					assert.equal(res.status, 201);
-				})
-				.catch((err) => {
-					throw err;
-				});
-		});
-	});
-	describe('GET', () => {
-		it('should return 200', async () => {
-			const image = await Image.findOne({ where: { user_id: user.id } });
-			await chai
-				.request(app)
-				.get(`/api/images/${image.id}`)
-				.set('Authorization', `Bearer ${auth}`)
-				.then((res) => {
-					assert.equal(res.status, 200);
-					assert.equal(res.header['content-type'], 'image/png');
-				})
-				.catch((err) => {
-					throw err;
-				});
-		});
-		it('should return 404 if no image is found with the given id', async () => {
-			await chai
-				.request(app)
-				.get('/api/images/0')
+				.get('/api/captions/0')
 				.set('Authorization', `Bearer ${auth}`)
 				.then((res) => {
 					assert.equal(res.status, 404);
@@ -106,12 +86,55 @@ describe('/images', async () => {
 		});
 	});
 
-	describe('DELETE', () => {
-		it('should return 204', async () => {
-			const image = await Image.findOne({ where: { user_id: user.id } });
+	describe('POST', () => {
+		it('should return 201', async () => {
 			await chai
 				.request(app)
-				.delete(`/api/images/${image.id}`)
+				.post(`/api/captions/${image.id}`)
+				.set('Authorization', `Bearer ${auth}`)
+				.send({
+					caption: 'test',
+				})
+				.then((res) => {
+					assert.equal(res.status, 201);
+				})
+				.catch((err) => {
+					throw err;
+				});
+		});
+		it('should return 401 if no token is provided', async () => {
+			await chai
+				.request(app)
+				.post(`/api/captions/${image.id}`)
+				.then((res) => {
+					assert.equal(res.status, 401);
+				})
+				.catch((err) => {
+					throw err;
+				});
+		});
+		it('should return 403 if there is already a caption to an image', async () => {
+			await chai
+				.request(app)
+				.post('/api/captions/0')
+				.set('Authorization', `Bearer ${auth}`)
+				.send({
+					caption: 'test',
+				})
+				.then((res) => {
+					assert.equal(res.status, 403);
+				})
+				.catch((err) => {
+					throw err;
+				});
+		});
+	});
+
+	describe('DELETE', () => {
+		it('should return 204', async () => {
+			await chai
+				.request(app)
+				.delete(`/api/captions/${image.id}`)
 				.set('Authorization', `Bearer ${auth}`)
 				.then((res) => {
 					assert.equal(res.status, 204);
@@ -120,10 +143,33 @@ describe('/images', async () => {
 					throw err;
 				});
 		});
-		it('should return 404 if no image is found with the given id', async () => {
+		it('should return 401 if no token is provided', async () => {
 			await chai
 				.request(app)
-				.delete('/api/images/0')
+				.delete(`/api/captions/${image.id}`)
+				.then((res) => {
+					assert.equal(res.status, 401);
+				})
+				.catch((err) => {
+					throw err;
+				});
+		});
+		it('should return 404 if caption does not exist', async () => {
+			await chai
+				.request(app)
+				.delete(`/api/captions/${image.id}`)
+				.set('Authorization', `Bearer ${auth}`)
+				.then((res) => {
+					assert.equal(res.status, 404);
+				})
+				.catch((err) => {
+					throw err;
+				});
+		});
+		it('should return 404 if image does not exist', async () => {
+			await chai
+				.request(app)
+				.delete('/api/captions/0')
 				.set('Authorization', `Bearer ${auth}`)
 				.then((res) => {
 					assert.equal(res.status, 404);
